3c106969ced806240b8f1c828a995703da3f35fe
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-01-25 22:20:08 +0530

    created react app and added Header Component

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# praveenorugantitech-tv-series-reactjs
-TV Series Search developed using React JS and Material UI
+# TV Series Search developed using React JS, TVMaze API and Material UI
+
+<div>
+<img align="right" width="100%"  src="./src/images/screenshot.PNG" />
+</div>
+
+
+[Live Demo](https://praveenorugantitech.github.io/praveenorugantitech-tv-series-reactjs/)
---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+const Header = () => {
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            color="inherit"
+            align="center"
+            style={{ flex: 1 }}
+          >
+            TV Series
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
